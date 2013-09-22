ee099936ca9a4c59b2e78c0740774260974b49f3
Leoung <alex.ged@live.fr>
2013-09-22 16:58:42 +0200

    Création du projet
    vide

---
+++ ProjetNexus/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "iostream";
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    std::cout<<"test";
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
